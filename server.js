2b20713ddf38ad43e466c886ba10611068a133c7
chudnofskyATD <mpereannor@gmail.com>
2019-10-30 13:31:20 +0000

    test endpoint passed

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
-const express = 'express';
-
+const express = require('express');
+const helmet = require('helmet');
 const server = express();
+
+server.use(helmet());
+server.use(express.json());
+
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`)
@@ -8,8 +12,11 @@ server.get('/', (req, res) => {
 
 //custom middleware
 
-function logger(req, res, next) {
+// function logger(req, res, next) {logger()
 
-};
+  //logger logs to the console the following information about each request: request method, request url, and a timestamp
+  //this middleware runs on every request made to the API
+
+//};
 
 module.exports = server;
